36b660a2a016eeab7ac62b82fe89bece983dcf14
ibtissemladjici50@gmail.com <ibtissemladjici50@gmail.com>
2024-04-18 16:00:11 +0100

    fixing the text

--- src/Scene.py
+++ src/Scene.py
@@ -21,17 +21,22 @@ class Scene:
 
         # Dark background for text
         pygame.draw.rect(screen, (0, 0, 0), (0, 310, 800, 90))
-
+  
         # Play sound if available
         if self.sound:
             celtic_song = pygame.mixer.Sound(self.sound)
             celtic_song.set_volume(1.0)
             celtic_song.play()
 
-        # Render text on the dark background
+        # Render multiline text on the dark background
         font = pygame.font.SysFont("Arial", 17)
-        text_surface = font.render(self.text, True, 'white')
-        screen.blit(text_surface, (10, 320))  # Adjusted y-coordinate for text
+        lines = self.text.split('\n')  # Split text into lines if it contains newline characters
+        y_offset = 320  # Initial y-coordinate for text
+        for line in lines:
+            text_surface = font.render(line, True, 'white')
+            screen.blit(text_surface, (10, y_offset))
+            y_offset += text_surface.get_height() + 5  # Adjust the vertical spacing between lines
+
 
     def handle_input(self, event,action_key_mapping):
         if event.type == pygame.KEYDOWN:
@@ -45,7 +50,7 @@ class Scene:
 class WoodsScene(Scene):
     def __init__(self):
         super().__init__(
-           "You are isolated in the woods ,surrounded by darkness and the distant  howls of wolves.",
+           "You are isolated in the woods ,surrounded \nby darkness and the distant  howls of wolves.",
            image=image1,
             actions=["next"],
            action_key_mapping = {
@@ -85,7 +90,7 @@ class BlackScene(Scene):
         
 class HauntedScene(Scene):
     def __init__(self):
-        super().__init__( "As you navigate through the haunted woods, strange sounds fill  the air, making you feel uneasy  "
+        super().__init__( "As you navigate through the haunted woods,\nstrange sounds fill  the air, making you feel uneasy  "
           ,image=image3 , actions=["next"],
         action_key_mapping = {
             
